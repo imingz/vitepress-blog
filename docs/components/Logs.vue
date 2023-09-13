7c8ddc5ec97046d347462f1a1f54e28bb01d7dfd
imimgz <imingz@qq.com>
2023-09-13 23:37:04 +0800

    fix: 修复日期选择不对的 bug

--- docs/components/Logs.vue
+++ docs/components/Logs.vue
@@ -33,17 +33,26 @@ onMounted(() => {
 // 初始化选中的标签和可选的标签选项
 const dateValue = ref("");
 
-// 监听选中的标签，根据选中的标签来更新相关文章列表
+// 监听选中的标签，根据选中的日期范围更新相关文章列表
 watch(dateValue, (newVal, oldVal) => {
-  relList.value = (
-    newVal
-      ? flatList.filter(
-          (item) =>
-            item.lastUpdated >= new Date(newVal[0]) &&
-            item.lastUpdated <= new Date(newVal[1])
-        )
-      : flatList
-  )
+  let res = [];
+
+  // 如果没有选中日期范围，则显示所有文章
+  if (!newVal) {
+    res = flatList;
+  } else {
+    // 如果选中了日期范围，则根据日期范围过滤文章
+    const start = new Date(newVal[0]);
+    start.setHours(0, 0, 0, 0);
+    const end = new Date(newVal[1]);
+    end.setHours(23, 59, 59, 999);
+    res = flatList.filter(
+      (item) => item.lastUpdated >= start && item.lastUpdated <= end
+    );
+  }
+
+  // 根据最后更新时间对列表进行排序
+  relList.value = res
     .sort((a, b) => b.lastUpdated.getTime() - a.lastUpdated.getTime())
     .map((item) => ({
       link: item.link,
