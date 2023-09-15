8e520707b45619046120eee45c1be2d768601ec3
imimgz <imingz@qq.com>
2023-09-15 18:00:16 +0800

    refactor: 移除 百度网页分析

--- docs/.vitepress/theme/components/NavBarTitleBefore.vue
+++ docs/.vitepress/theme/components/NavBarTitleBefore.vue
@@ -4,7 +4,6 @@
 import { inject, ref, onMounted, Ref } from "vue";
 import axios from "axios";
 import { ElNotification } from "element-plus";
-import ChatDotRound from "./svg/ChatDotRound.vue";
 
 // 随机句子
 const url = "https://v1.hitokoto.cn/";
@@ -51,24 +50,13 @@ onMounted(async () => {
   });
 });
 
-// 百度网页分析
-const DEV = inject("DEV") as Ref<boolean>;
-
-var _hmt = _hmt || [];
-
-const visitorAnalytics = () => {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?442f2aacc945fba2a6de01afd93727a7";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode?.insertBefore(hm, s);
-};
-
 // vercel 网页分析
 import { inject as vercelAnalytics } from "@vercel/analytics";
 
+const DEV = inject("DEV") as Ref<boolean>;
+
 onMounted(() => {
   if (!DEV) {
-    visitorAnalytics();
     vercelAnalytics();
   }
 });
