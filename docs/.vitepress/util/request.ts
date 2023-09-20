16e5e162472dfae9c65a9f9ebe417dd9db18565c
imingz <imingz@qq.com>
2023-09-20 22:58:01 +0800

    fix: 支持 https

--- docs/.vitepress/util/request.ts
+++ docs/.vitepress/util/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://a.imingz.fun", // url = base url + request url
+  baseURL: "https://a.imingz.fun", // url = base url + request url
   timeout: 5000, // request timeout
 });
 
