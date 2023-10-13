01c3f5ab5b74162c79972fd23a832b6369dfeb96
imingz <imingz@qq.com>
2023-10-13 17:04:42 +0800

    摸鱼改造完成

--- docs/components/Logs.vue
+++ docs/components/Logs.vue
@@ -4,10 +4,10 @@ import { ElTable, ElTableColumn, ElDatePicker } from "element-plus";
 import { getFlatList, getUrlParam, getReadableTime } from "./utils/common";
 
 type resType = {
-  lastUpdated: string;
-  link: string;
-  text: string;
-  tags?: string[];
+    lastUpdated: string;
+    link: string;
+    text: string;
+    tags?: string[];
 };
 
 // 获取扁平化的列表
@@ -17,106 +17,106 @@ const relList: Ref<resType[]> = ref([]);
 
 // 在组件挂载后
 onMounted(() => {
-  // 标签
-  const tag = getUrlParam("tag");
-  let li: listItem[];
-  if (tag) {
-    tagsValue.value = [tag];
-    li = flatList.filter((item) => item.tags && item.tags.includes(tag));
-  } else {
-    li = flatList;
-  }
-  // 根据最后更新时间对列表进行排序
-  relList.value = listSort(li);
+    // 标签
+    const tag = getUrlParam("tag");
+    let li: listItem[];
+    if (tag) {
+        tagsValue.value = [tag];
+        li = flatList.filter((item) => item.tags && item.tags.includes(tag));
+    } else {
+        li = flatList;
+    }
+    // 根据最后更新时间对列表进行排序
+    relList.value = listSort(li);
 });
 
 const listSort = (list: listItem[]): resType[] => {
-  return list
-    .sort((a, b) => b.lastUpdated.getTime() - a.lastUpdated.getTime())
-    .map((item) => ({
-      ...item,
-      lastUpdated: getReadableTime(item.lastUpdated),
-    }));
+    return list
+        .sort((a, b) => b.lastUpdated.getTime() - a.lastUpdated.getTime())
+        .map((item) => ({
+            ...item,
+            lastUpdated: getReadableTime(item.lastUpdated),
+        }));
 };
 
 // 初始化选中的标签和可选的标签选项
 const dateValue = ref("");
 const shortcuts = [
-  {
-    text: "🐼 今天",
-    value: () => {
-      const end = new Date();
-      const start = new Date();
-      return [start, end];
+    {
+        text: "🐼 今天",
+        value: () => {
+            const end = new Date();
+            const start = new Date();
+            return [start, end];
+        },
     },
-  },
-  {
-    text: "过去一周",
-    value: () => {
-      const end = new Date();
-      const start = new Date();
-      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-      return [start, end];
+    {
+        text: "过去一周",
+        value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            return [start, end];
+        },
     },
-  },
-  {
-    text: "过去一月",
-    value: () => {
-      const end = new Date();
-      const start = new Date();
-      start.setMonth(start.getMonth() - 1);
-      return [start, end];
+    {
+        text: "过去一月",
+        value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setMonth(start.getMonth() - 1);
+            return [start, end];
+        },
     },
-  },
-  {
-    text: "过去三月",
-    value: () => {
-      const end = new Date();
-      const start = new Date();
-      start.setMonth(start.getMonth() - 3);
-      return [start, end];
+    {
+        text: "过去三月",
+        value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setMonth(start.getMonth() - 3);
+            return [start, end];
+        },
     },
-  },
-  {
-    text: "过去半年",
-    value: () => {
-      const end = new Date();
-      const start = new Date();
-      start.setMonth(start.getMonth() - 6);
-      return [start, end];
+    {
+        text: "过去半年",
+        value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setMonth(start.getMonth() - 6);
+            return [start, end];
+        },
     },
-  },
-  {
-    text: "过去一年",
-    value: () => {
-      const end = new Date();
-      const start = new Date();
-      start.setFullYear(start.getFullYear() - 1);
-      return [start, end];
+    {
+        text: "过去一年",
+        value: () => {
+            const end = new Date();
+            const start = new Date();
+            start.setFullYear(start.getFullYear() - 1);
+            return [start, end];
+        },
     },
-  },
 ];
 
 // 监听选中的标签，根据选中的日期范围更新相关文章列表
 watch(dateValue, (newVal, oldVal) => {
-  let res = [];
+    let res = [];
 
-  // 如果没有选中日期范围，则显示所有文章
-  if (!newVal) {
-    res = flatList;
-  } else {
-    // 如果选中了日期范围，则根据日期范围过滤文章
-    const start = new Date(newVal[0]);
-    start.setHours(0, 0, 0, 0);
-    const end = new Date(newVal[1]);
-    end.setHours(23, 59, 59, 999);
-    res = flatList.filter(
-      (item) => item.lastUpdated >= start && item.lastUpdated <= end
-    );
-  }
+    // 如果没有选中日期范围，则显示所有文章
+    if (!newVal) {
+        res = flatList;
+    } else {
+        // 如果选中了日期范围，则根据日期范围过滤文章
+        const start = new Date(newVal[0]);
+        start.setHours(0, 0, 0, 0);
+        const end = new Date(newVal[1]);
+        end.setHours(23, 59, 59, 999);
+        res = flatList.filter(
+            (item) => item.lastUpdated >= start && item.lastUpdated <= end
+        );
+    }
 
-  // 根据最后更新时间对列表进行排序
-  relList.value = listSort(res);
+    // 根据最后更新时间对列表进行排序
+    relList.value = listSort(res);
 });
 
 const tagsValue: Ref<string[]> = ref([]);
@@ -124,87 +124,87 @@ const tags = flatList.flatMap((item) => item.tags || []);
 const uniqueTag = Array.from(new Set(tags));
 const options = uniqueTag.map((tag) => ({ text: tag, value: tag }));
 const filterTag = (value: string, row: listItem) => {
-  return row.tags && row.tags.includes(value);
+    return row.tags && row.tags.includes(value);
 };
 
 // 监听选中的标签，根据选中的标签来更新相关文章列表
 watch(tagsValue, (newVal, oldVal) => {
-  let li = flatList;
-  for (const tag of newVal) {
-    li = li.filter((item) => item.tags && item.tags.includes(tag));
-  }
-  relList.value = listSort(li);
+    let li = flatList;
+    for (const tag of newVal) {
+        li = li.filter((item) => item.tags && item.tags.includes(tag));
+    }
+    relList.value = listSort(li);
 });
 </script>
 
 <template>
-  <div class="log-page">
-    <slot></slot>
-    <!-- 标签选择器 -->
-    <el-select
-      v-model="tagsValue"
-      multiple
-      filterable
-      style="width: 100%"
-      placeholder="选择想要筛选的标签"
-      class="tag-filter">
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :value="item.value" />
-    </el-select>
-    日期筛选：从
-    <el-date-picker
-      v-model="dateValue"
-      type="daterange"
-      unlink-panels
-      range-separator="到"
-      start-placeholder="开始时间"
-      end-placeholder="结束时间"
-      :shortcuts="shortcuts" />
-    <el-table :data="relList" stripe style="width: 100%">
-      <el-table-column prop="lastUpdated" label="更新时间" sortable />
-      <el-table-column prop="text" label="标题" sortable />
-      <el-table-column
-        prop="tags"
-        label="标签"
-        sortable
-        :filters="options"
-        :filter-method="filterTag" />
-      <el-table-column prop="link" label="链接">
-        <template #default="{ row }">
-          <a :href="row.link" class="tag-link">{{
-            row.link
-              .replace(/[0-9]+-/g, "")
-              .replace("/", "")
-              .split("/")
-              .reverse()
-              .slice(1)
-              .join(" <-- ")
-          }}</a>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+    <div class="log-page">
+        <slot></slot>
+        <!-- 标签选择器 -->
+        <el-select
+            v-model="tagsValue"
+            multiple
+            filterable
+            style="width: 100%"
+            placeholder="选择想要筛选的标签"
+            class="tag-filter">
+            <el-option
+                v-for="item in options"
+                :key="item.value"
+                :value="item.value" />
+        </el-select>
+        日期筛选：从
+        <el-date-picker
+            v-model="dateValue"
+            type="daterange"
+            unlink-panels
+            range-separator="到"
+            start-placeholder="开始时间"
+            end-placeholder="结束时间"
+            :shortcuts="shortcuts" />
+        <el-table :data="relList" stripe style="width: 100%">
+            <el-table-column prop="lastUpdated" label="更新时间" sortable />
+            <el-table-column prop="text" label="标题" sortable />
+            <el-table-column
+                prop="tags"
+                label="标签"
+                sortable
+                :filters="options"
+                :filter-method="filterTag" />
+            <el-table-column prop="link" label="链接">
+                <template #default="{ row }">
+                    <a :href="row.link" class="tag-link">{{
+                        row.link
+                            .replace(/[0-9]+-/g, "")
+                            .replace("/", "")
+                            .split("/")
+                            .reverse()
+                            .slice(1)
+                            .join(" <-- ")
+                    }}</a>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <style lang="scss">
 .log-page {
-  @media (min-width: 414px) {
-    width: 60%;
-    margin: 0 auto;
-  }
-  .tag-filter {
-    padding-top: 16px;
-    padding-bottom: 16px;
-  }
-  .tag-link {
-    color: var(--vp-c-brand-1);
-  }
+    @media (min-width: 414px) {
+        width: 60%;
+        margin: 0 auto;
+    }
+    .tag-filter {
+        padding-top: 16px;
+        padding-bottom: 16px;
+    }
+    .tag-link {
+        color: var(--vp-c-brand-1);
+    }
 }
 .dark {
-  .tag-link {
-    color: var(--vp-c-brand-lighter);
-  }
+    .tag-link {
+        color: var(--vp-c-brand-lighter);
+    }
 }
 </style>
--- docs/components/MoYu.vue
+++ docs/components/MoYu.vue
@@ -2,29 +2,30 @@
 import { ref, onMounted, Ref } from "vue";
 import service from "./utils/request";
 import { ElNotification } from "element-plus";
+import { da } from "element-plus/es/locale";
 
 interface Payday {
-  发薪时间: number;
-  距离天数: number;
+    paydayTime: number;
+    daysUntil: number;
 }
 
 interface Holiday {
-  节日名称: string;
-  距离天数: number;
+    holidayName: string;
+    daysUntil: number;
 }
 
 interface MoYu {
-  单休: number;
-  双休: number;
-  发薪日: Payday[];
-  节假日: Holiday[];
+    singleDayOff: number;
+    doubleDayOff: number;
+    paydayList: Payday[];
+    holidayList: Holiday[];
 }
 
 const moyu: Ref<MoYu> = ref({
-  单休: 0,
-  双休: 0,
-  发薪日: [],
-  节假日: [],
+    singleDayOff: 0,
+    doubleDayOff: 0,
+    paydayList: [],
+    holidayList: [],
 });
 
 const today = new Date();
@@ -32,113 +33,125 @@ const year = today.getFullYear();
 const month = today.getMonth() + 1;
 const day = today.getDate();
 const week = [
-  "星期日",
-  "星期一",
-  "星期二",
-  "星期三",
-  "星期四",
-  "星期五",
-  "星期六",
+    "星期日",
+    "星期一",
+    "星期二",
+    "星期三",
+    "星期四",
+    "星期五",
+    "星期六",
 ][today.getDay()];
 
 const todayText = `今天是 ${year} 年 ${month} 月 ${day} 日，${week}`;
 
 onMounted(() => {
-  service
-    .get("/moyu")
-    .then((response) => {
-      moyu.value = response.data;
-    })
-    .catch((error) => {
-      if (error.code === "ECONNABORTED") {
-        ElNotification({
-          message: "请求超时",
-          type: "error",
+    service
+        .get("/v1/moyu")
+        .then((response) => {
+            const data = response.data;
+            if (data.code !== 0) {
+                ElNotification({
+                    message: "服务错误：" + data.message,
+                    type: "error",
+                });
+                console.log("服务错误：" + data.message);
+            }
+            moyu.value = data.data;
+        })
+        .catch((error) => {
+            if (error.code === "ECONNABORTED") {
+                ElNotification({
+                    message: "请求超时",
+                    type: "error",
+                });
+            } else {
+                console.error("请求发生错误:", error);
+                ElNotification({
+                    message: "请求发生错误",
+                    type: "error",
+                });
+            }
         });
-      } else {
-        console.error("请求发生错误:", error);
-        ElNotification({
-          message: "请求发生错误",
-          type: "error",
-        });
-      }
-    });
 });
 </script>
 
 <template>
-  <div class="moyu-page">
-    <div class="moyu-hello">
-      <el-card>
-        <p>{{ todayText }}</p>
-        <p>早安，摸鱼人！</p>
-        <p>工作再忙一定不要忘记休息哦！</p>
-        <p>
-          起身去茶水间，去厕所走走，钱是老板的但命是自己的，祝愿摸鱼人愉快的渡过每一天…
-        </p>
-      </el-card>
+    <div class="moyu-page">
+        <div class="moyu-hello">
+            <el-card>
+                <p>{{ todayText }}</p>
+                <p>早安，摸鱼人！</p>
+                <p>工作再忙一定不要忘记休息哦！</p>
+                <p>
+                    起身去茶水间，去厕所走走，钱是老板的但命是自己的，祝愿摸鱼人愉快的渡过每一天…
+                </p>
+            </el-card>
+        </div>
+        <div class="moyu-holiday">
+            <el-card>
+                <template #header>
+                    <div>
+                        <span> 距离节假日 🏖</span>
+                    </div>
+                </template>
+                <p>
+                    【周六（双休）】：{{
+                        moyu.doubleDayOff
+                            ? `${moyu.doubleDayOff}天 `
+                            : "好好享受假期吧！"
+                    }}
+                </p>
+                <p>
+                    【周日（单休）】：{{
+                        moyu.singleDayOff
+                            ? `${moyu.singleDayOff}天 `
+                            : "好好享受假期吧！"
+                    }}
+                </p>
+                <p v-for="i in moyu.holidayList" :key="i.holidayName">
+                    【{{ i.holidayName }}】：{{ i.daysUntil }}天
+                </p>
+            </el-card>
+        </div>
+        <div class="moyu-salary">
+            <el-card>
+                <template #header>
+                    <div>
+                        <span> 距离发薪日💰</span>
+                    </div>
+                </template>
+                <p v-for="i in moyu.paydayList" :key="i.paydayTime">
+                    【{{ i.paydayTime }}号】：{{ i.daysUntil }}天
+                </p>
+            </el-card>
+        </div>
     </div>
-    <div class="moyu-holiday">
-      <el-card>
-        <template #header>
-          <div>
-            <span> 距离节假日 🏖</span>
-          </div>
-        </template>
-        <p>
-          【周六（双休）】：{{
-            moyu.双休 ? `${moyu.双休}天 ` : "好好享受假期吧！"
-          }}
-        </p>
-        <p>
-          【周日（单休）】：{{
-            moyu.单休 ? `${moyu.单休}天 ` : "好好享受假期吧！"
-          }}
-        </p>
-        <p v-for="i in moyu.节假日" :key="i.节日名称">
-          【{{ i.节日名称 }}】：{{ i.距离天数 }}天
-        </p>
-      </el-card>
-    </div>
-    <div class="moyu-salary">
-      <el-card>
-        <template #header>
-          <div>
-            <span> 距离发薪日💰</span>
-          </div>
-        </template>
-        <p v-for="i in moyu.发薪日" :key="i.发薪时间">
-          【{{ i.发薪时间 }}号】：{{ i.距离天数 }}天
-        </p>
-      </el-card>
-    </div>
-  </div>
 </template>
 
 <style scoped>
 .moyu-page {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-auto-rows: auto;
-  grid-column-gap: 14px;
-  grid-row-gap: 14px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: auto;
+    grid-column-gap: 14px;
+    grid-row-gap: 14px;
 }
 
 .moyu-hello {
-  grid-area: 1 / 1 / 2 / 3;
+    grid-area: 1 / 1 / 2 / 3;
 }
 .moyu-holiday {
-  grid-area: 2 / 1 / 4 / 2;
+    grid-area: 2 / 1 / 4 / 2;
 
-  @media (max-width: 414px) {
-    grid-area: 2 / 1 / 3 / 3;
-  }
+    @media (max-width: 414px) {
+        grid-area: 2 / 1 / 3 / 3;
+    }
 }
 .moyu-salary {
-  grid-area: 2 / 2 / 4 / 3;
+    grid-area: 2 / 2 / 4 / 3;
 
-  @media (max-width: 414px) {
-    grid-area: 3 / 1 / 3 / 3;
-  }
+    @media (max-width: 414px) {
+        grid-area: 3 / 1 / 3 / 3;
+    }
 }
 </style>
