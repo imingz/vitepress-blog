84289520ef426f475b7be665e894428d4d1b04f5
imingz <imingz@qq.com>
2023-10-13 17:12:04 +0800

    refactor: 改造短链

--- docs/.vitepress/theme/components/share.vue
+++ docs/.vitepress/theme/components/share.vue
@@ -1,7 +1,7 @@
 <template>
-  <div @click="openShare">
-    <Share class="share" />
-  </div>
+    <div @click="openShare">
+        <Share class="share" />
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -16,70 +16,70 @@ const host = "imingz.fun"; //window.location.host;
 const route = useRoute();
 
 const url = computed(() => {
-  const { path } = route;
-  return protocol + host + path;
+    const { path } = route;
+    return protocol + host + path;
 });
 
 const sUrl = ref("");
 
 const openShare = async () => {
-  await service
-    .get("/shorturl?url=" + url.value, { timeout: 5000 })
-    .then((response) => {
-      sUrl.value = response.data;
-      copyToClipboard(sUrl.value);
-    })
-    .catch((error) => {
-      if (error.code === "ECONNABORTED") {
-        ElNotification({
-          message: "请求超时",
-          type: "error",
+    await service
+        .get("/v1/shortLink?url=" + url.value, { timeout: 5000 })
+        .then((response) => {
+            sUrl.value = response.data.data.shorted_url;
+            copyToClipboard(sUrl.value);
+        })
+        .catch((error) => {
+            if (error.code === "ECONNABORTED") {
+                ElNotification({
+                    message: "请求超时",
+                    type: "error",
+                });
+            } else {
+                console.error("请求发生错误:", error);
+                ElNotification({
+                    message: "请求发生错误",
+                    type: "error",
+                });
+            }
         });
-      } else {
-        console.error("请求发生错误:", error);
-        ElNotification({
-          message: "请求发生错误",
-          type: "error",
-        });
-      }
-    });
 };
 
 function copyToClipboard(text: string): Promise<void> {
-  return navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      ElNotification({
-        message: "链接已复制到剪贴板",
-        type: "success",
-      });
-    })
-    .catch(() => {
-      ElNotification({
-        duration: 10000,
-        message: "复制链接到剪贴板失败，请手动复制: " + text,
-        type: "warning",
-      });
-    });
+    return navigator.clipboard
+        .writeText(text)
+        .then(() => {
+            ElNotification({
+                message: "链接已复制到剪贴板",
+                type: "success",
+            });
+        })
+        .catch(() => {
+            ElNotification({
+                duration: 10000,
+                message: "复制链接到剪贴板失败，请手动复制: " + text,
+                type: "warning",
+            });
+        });
 }
 </script>
 
 <style lang="scss">
 .share {
-  width: 1.3em;
-  height: 1.3em;
-  @media (min-width: 767px) {
-    margin-left: 1em;
-  }
-  &:hover {
-    color: var(--vp-c-brand-1);
-  }
+    width: 1.3em;
+    height: 1.3em;
+    @media (min-width: 767px) {
+        margin-left: 1em;
+    }
+    &:hover {
+        color: var(--vp-c-brand-1);
+    }
 }
 .dark {
-  .share {
-    &:hover {
-      color: var(--vp-c-brand-lighter);
+    .share {
+        &:hover {
+            color: var(--vp-c-brand-lighter);
+        }
     }
-  }
 }
 </style>
--- docs/components/ShortLink.vue
+++ docs/components/ShortLink.vue
@@ -2,11 +2,11 @@
 import { reactive } from "vue";
 import { useData } from "vitepress";
 import {
-  ElInput,
-  ElForm,
-  ElFormItem,
-  ElButton,
-  ElNotification,
+    ElInput,
+    ElForm,
+    ElFormItem,
+    ElButton,
+    ElNotification,
 } from "element-plus";
 import service from "./utils/request";
 import { copyToClipboard } from "./utils/util";
@@ -14,63 +14,63 @@ import { copyToClipboard } from "./utils/util";
 const { isDark } = useData();
 
 const form = reactive({
-  oUrl: "",
-  sUrl: "",
+    oUrl: "",
+    sUrl: "",
 });
 
 const shortURL = () => {
-  service
-    .get("/shorturl?url=" + form.oUrl)
-    .then((response) => {
-      form.sUrl = response.data;
-      copyToClipboard(response.data);
-    })
-    .catch((error) => {
-      if (error.code === "ECONNABORTED") {
-        ElNotification({
-          message: "请求超时",
-          type: "error",
+    service
+        .get("/v1/shortLink?url=" + form.oUrl)
+        .then((response) => {
+            form.sUrl = response.data.data.shorted_url;
+            copyToClipboard(response.data.data.shorted_url);
+        })
+        .catch((error) => {
+            if (error.code === "ECONNABORTED") {
+                ElNotification({
+                    message: "请求超时",
+                    type: "error",
+                });
+            } else {
+                console.error("请求发生错误:", error);
+                ElNotification({
+                    message: "请求发生错误",
+                    type: "error",
+                });
+            }
         });
-      } else {
-        console.error("请求发生错误:", error);
-        ElNotification({
-          message: "请求发生错误",
-          type: "error",
-        });
-      }
-    });
 };
 </script>
 
 <template>
-  <div>
-    <el-form :model="form" label-width="120px">
-      <el-form-item label="源网址: ">
-        <el-input v-model="form.oUrl" placeholder="源网址" />
-      </el-form-item>
-      <el-form-item>
-        <el-button
-          plain
-          round
-          color="var(--vp-c-brand-1)"
-          :dark="isDark"
-          @click="shortURL">
-          生成
-        </el-button>
-        <el-button
-          plain
-          round
-          color="var(--vp-c-brand-1)"
-          :dark="isDark"
-          @click="copyToClipboard(form.sUrl)">
-          复制
-        </el-button>
-      </el-form-item>
-      <el-form-item label="短网址:">
-        {{ form.sUrl }}
-      </el-form-item>
-    </el-form>
-  </div>
+    <div>
+        <el-form :model="form" label-width="120px">
+            <el-form-item label="源网址: ">
+                <el-input v-model="form.oUrl" placeholder="源网址" />
+            </el-form-item>
+            <el-form-item>
+                <el-button
+                    plain
+                    round
+                    color="var(--vp-c-brand-1)"
+                    :dark="isDark"
+                    @click="shortURL">
+                    生成
+                </el-button>
+                <el-button
+                    plain
+                    round
+                    color="var(--vp-c-brand-1)"
+                    :dark="isDark"
+                    @click="copyToClipboard(form.sUrl)">
+                    复制
+                </el-button>
+            </el-form-item>
+            <el-form-item label="短网址:">
+                {{ form.sUrl }}
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <style lang="scss"></style>
